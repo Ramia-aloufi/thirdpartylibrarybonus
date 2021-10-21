1746261e00cdcd6bb4ea4517a5e18b094110ab5f
RAM111111 <r.r.99@windowslive.com>
2021-10-22 01:16:02 +0300

    f commit

---
+++ app/src/main/java/com/example/thirdpartylibrarybonus/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.thirdpartylibrarybonus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+import android.R
+import android.graphics.Color
+import android.widget.ProgressBar
+import com.skydoves.progressview.ProgressView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.thirdpartylibrarybonus.R.layout.main_activity)
+
+
+
+
+
+
+    }
+}
